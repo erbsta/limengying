d76a2e7127338a488ca053eaa275a15c77861269
LMY <guests@LindeAir.lan>
2015-02-01 17:20:21 +0800

    day1

---
+++ Day1/playground.playground/section-1.swift
@@ -0,0 +1,47 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+/*var str = "Hello, playground"
+
+let constant = 3
+
+11/3
+
+var a: String!
+
+for var i = 1; i < 10; i = i + 1 {
+    println(i)
+}
+*/
+
+var array = [1,2,3]
+
+array[0]
+
+for x in array{
+    println(x)
+    if x == 2{
+        break;
+        x+1
+    }
+}
+
+
+/*array.append(4)
+
+array
+
+var arr = [1,4,5,6,7]
+
+arr.removeRange(1...3)
+
+var dictionary = ["Name": "Tang", "Age": 23, "Fvaor": "Sweet"]
+
+dictionary["Name"]!
+
+var s = "Welcome to OWL" as NSString
+
+s.substringToIndex(7)
+
+dictionary["Age"]*/
